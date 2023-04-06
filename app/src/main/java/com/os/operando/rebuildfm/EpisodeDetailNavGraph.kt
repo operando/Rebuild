dc12d7156299e338f616bc3b0f0ce6c8b51edc26
Shinobu Okano <okanoshinobu8@gmail.com>
2023-04-06 15:11:07 +0900

    エピソード再生できるようにした

--- app/src/main/java/com/os/operando/rebuildfm/EpisodeDetailNavGraph.kt
+++ app/src/main/java/com/os/operando/rebuildfm/EpisodeDetailNavGraph.kt
@@ -1,12 +1,10 @@
 package com.os.operando.rebuildfm
 
+import android.content.Context
 import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
@@ -16,6 +14,12 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import java.io.File
 
 fun NavGraphBuilder.episodeDetailNavGraph() {
     composable(
@@ -31,6 +35,10 @@ fun NavGraphBuilder.episodeDetailNavGraph() {
 fun EpisodeDetail(viewModel: EpisodeDetailViewModel = hiltViewModel()) {
     viewModel.getEpisode(LocalContext.current)
     val episode = viewModel.episode.observeAsState().value
+    val audio = viewModel.audio.observeAsState().value
+    if (audio != null) {
+        initPlayer(LocalContext.current, buildMediaSource(LocalContext.current, audio))
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -40,10 +48,47 @@ fun EpisodeDetail(viewModel: EpisodeDetailViewModel = hiltViewModel()) {
     ) {
         Column(modifier = Modifier.padding(it)) {
             Text(text = episode?.itunes?.subtitle ?: "")
+            Button(onClick = {
+                audioPlayer?.playWhenReady = true
+            }) {
+                Text(text = "Click")
+            }
         }
 
     }
 }
+
+private var audioPlayer: ExoPlayer? = null
+
+private val playerEventListener = object : Player.Listener {
+}
+
+private fun buildMediaSource(context: Context, file: File): MediaSource? {
+    return try {
+        val uri = Uri.fromFile(file)
+        val factory = DefaultDataSource.Factory(
+            context,
+        )
+        ProgressiveMediaSource.Factory(factory)
+            .createMediaSource(MediaItem.Builder().setUri(uri).build())
+    } catch (e: Exception) {
+        null
+    }
+}
+
+private fun initPlayer(context: Context, mediaSource: MediaSource?) {
+    mediaSource ?: return
+    val trackSelector = DefaultTrackSelector(context)
+    val loadControl = DefaultLoadControl()
+    audioPlayer =
+        ExoPlayer.Builder(context).setTrackSelector(trackSelector).setLoadControl(loadControl)
+            .build()
+    audioPlayer?.setMediaSource(mediaSource)
+    audioPlayer?.prepare()
+    audioPlayer?.addListener(playerEventListener)
+    audioPlayer?.playWhenReady = false
+}
+
 
 object EpisodeDetailNavGraph {
     const val episodeDetailRoute = "episode_detail/{episodeId}"
--- app/src/main/java/com/os/operando/rebuildfm/EpisodeDetailViewModel.kt
+++ app/src/main/java/com/os/operando/rebuildfm/EpisodeDetailViewModel.kt
@@ -7,7 +7,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.stalla.model.Episode
 import kotlinx.coroutines.launch
 import okhttp3.*
-import okio.*
+import okio.BufferedSink
+import okio.IOException
+import okio.buffer
+import okio.sink
 import java.io.File
 import javax.inject.Inject
 
@@ -18,6 +21,8 @@ class EpisodeDetailViewModel @Inject constructor(
 ) : ViewModel() {
     private val _episode = MutableLiveData<Episode?>()
     val episode: LiveData<Episode?> = _episode
+    private val _audio = MutableLiveData<File?>()
+    val audio: LiveData<File?> = _audio
     private var episodeId: String = ""
 
     init {
@@ -31,6 +36,13 @@ class EpisodeDetailViewModel @Inject constructor(
             val episode = rssFeedRepository.getEpisode(episodeId)
             _episode.postValue(episode)
 
+            val file = File(context.cacheDir, "$episodeId.mp3")
+            if (file.exists()) {
+                println("file.exists")
+                _audio.postValue(file)
+                return@launch
+            }
+
             val request = Request.Builder().apply {
                 url(episode!!.enclosure.url)
                 get()
@@ -41,10 +53,12 @@ class EpisodeDetailViewModel @Inject constructor(
                 }
 
                 override fun onResponse(call: Call, response: Response) {
-                    val file = File(context.cacheDir, "test.mp3")
+                    val file = File(context.cacheDir, "$episodeId.mp3")
                     val sink: BufferedSink = file.sink().buffer()
                     response.body?.source()?.let { sink.writeAll(it) }
                     sink.close()
+
+                    _audio.postValue(file)
 
                     val mp3File = Mp3File(file)
 
